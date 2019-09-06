3db7dc94f2d888acb5d2cafd1d6ad3c3aa37e885
Nitin Reddy <reddynitin3@gmail.com>
2019-09-06 17:28:30 +0530

    Type conversion

---
+++ dataTypeProgram.dart
@@ -0,0 +1,25 @@
+main(){
+  /*
+  int
+  String
+  bool
+  double
+  dynamic
+  */
+
+  int number1 = 100;
+  var number2 = 200;
+
+  print('Number 1: $number1 | Number 2: $number2');
+
+  String firstName = 'Nitin';
+  var lastName = 'Reddy';
+
+  print('First name: $firstName | Last name: $lastName');
+
+  bool isTrue = true;
+  var isFalse = false;
+
+  print('Boolean value 1: $isTrue | Boolean value 2: $isFalse');
+
+}
---
+++ typeConversion.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+main() {
+  // String -> int
+  var one = int.parse('1');
+  // var assertValue = assert(one == 1);
+  assert(one == 1);
+
+  // print('Assertion -> $assertValue');
+
+  // String -> double
+  var onePointOne = double.parse('1.1');
+  // var assertDoubleValue = assert(onePointOne == 1.1);
+  assert(onePointOne == 1.1);
+  // print('Assertion -> $assertDoubleValue');
+}
